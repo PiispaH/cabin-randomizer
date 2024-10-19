74fbdeba80301948a1e4ef8fd55ae2bc6e20eaae
Henrik Koski <henrikkoski00@gmail.com>
2024-10-19 18:51:30 +0300

    Add docstring and improve typing

--- hytti_arvonta/cabins.py
+++ hytti_arvonta/cabins.py
@@ -1,8 +1,17 @@
 #!/usr/bin/env python3
 
 from random import shuffle
+from typing import List
 
-def cabins(n: int, names: list):
+def cabins(n: int, names: list) -> List[List]:
+    """
+    Randomizes the given names into n groups of four each.
+
+    Args:
+        n (int): Amount of cabins.
+    Returns:
+        rooms (list): A list of the randomized four person cabins.
+    """
     if not isinstance(n, int) or n < 1:
         raise ValueError("n must be an integer with value greater than 0.")
     if len(names) != 4 * n:
